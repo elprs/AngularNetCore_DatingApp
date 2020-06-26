d148f027194b1522937e1f1d2c45b0676aff6567
elprs <eleni.parisi.sunshine@gmail.com>
2020-06-26 10:07:25 +0300

    connectionString

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,8 @@ namespace AngularNetCore_DatingApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlite("ConnectionString"));
+            services.AddDbContext<DataContext>(x => x.UseSqlite
+            (Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
         }
