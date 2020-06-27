3aa978ece84409618d97e61cb8f9c08663e882ef
elprs <eleni.parisi.sunshine@gmail.com>
2020-06-27 11:54:06 +0300

    Making Get Function synchronus

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AngularNetCore_DatingApp.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
@@ -23,17 +24,17 @@ namespace DatingApp.API.Controllers
          
         // GET api/values
         [HttpGet]
-        public IActionResult GetValues()
+        public async Task<IActionResult> GetValues()
         {
-            var values = _context.Values.ToList();
-            return Ok(values);
+            var values = await _context.Values.ToListAsync();
+            return  Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public IActionResult GetValue(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
             return Ok(value);
         }
 
